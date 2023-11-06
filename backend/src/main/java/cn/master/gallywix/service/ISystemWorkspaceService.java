eac3c67ce3352c80a0afb76d4b793040e15934ab
jingll <the2ndindec@hotmail.com>
2023-11-06 09:05:17 +0800

    :recycle: 获取数据时使用post方法

--- backend/src/main/java/cn/master/gallywix/service/ISystemWorkspaceService.java
+++ backend/src/main/java/cn/master/gallywix/service/ISystemWorkspaceService.java
@@ -1,7 +1,9 @@
 package cn.master.gallywix.service;
 
-import com.mybatisflex.core.service.IService;
+import cn.master.gallywix.controller.vo.workspace.WorkspacePageReqVO;
 import cn.master.gallywix.entity.SystemWorkspace;
+import com.mybatisflex.core.paginate.Page;
+import com.mybatisflex.core.service.IService;
 
 /**
  * 工作空间信息 服务层。
@@ -12,4 +14,6 @@ import cn.master.gallywix.entity.SystemWorkspace;
 public interface ISystemWorkspaceService extends IService<SystemWorkspace> {
 
     SystemWorkspace add(SystemWorkspace systemWorkspace);
+
+    Page<SystemWorkspace> findDataByPage(WorkspacePageReqVO page);
 }
