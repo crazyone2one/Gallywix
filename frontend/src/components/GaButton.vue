3d4e16ac0e94940609273e5429e58c89559c0169
jingll <the2ndindec@hotmail.com>
2023-11-09 12:47:59 +0800

    公共组件

---
+++ frontend/src/components/GaButton.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { NButton, NIcon, NPopover } from "naive-ui"
+
+interface PROPS {
+  size?: "tiny" | "small" | "medium" | "large"
+  type?: "default" | "tertiary" | "primary" | "success" | "info" | "warning" | "error"
+  text?: boolean
+  circle?: boolean
+  iconClass?: string
+  isIcon?: boolean
+  buttonText?: string
+  isPop?: boolean
+  popText?: string
+}
+
+withDefaults(defineProps<PROPS>(), {
+  size: "small",
+  type: "primary",
+  text: false,
+  circle: false,
+  iconClass: "i-tabler:question-circle",
+  isIcon: false,
+  buttonText: "ops",
+  isPop: false,
+  popText: "ops",
+})
+const emits = defineEmits(["exec"])
+</script>
+
+<template>
+  <n-popover v-if="isPop" trigger="hover">
+    <template #trigger>
+      <n-button
+        :size="size"
+        :type="type"
+        :text="text"
+        :circle="circle"
+        :style="type ? 'font-size:20px' : ''"
+        @click="emits('exec')">
+        <n-icon v-if="text && isIcon">
+          <span :class="iconClass" />
+        </n-icon>
+        <span v-else>{{ buttonText }}</span>
+      </n-button>
+    </template>
+    <span>{{ popText }}</span>
+  </n-popover>
+  <n-button
+    v-else
+    :size="size"
+    :type="type"
+    :text="text"
+    :circle="circle"
+    :style="type ? 'font-size:20px' : ''"
+    @click="emits('exec')">
+    <n-icon v-if="text && isIcon">
+      <span :class="iconClass" />
+    </n-icon>
+    <span v-else>{{ buttonText }}</span>
+  </n-button>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/table/GaTableOperation.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { NSpace } from "naive-ui"
+import GaButton from "/@/components/GaButton.vue"
+interface PROPS {
+  showEdit?: boolean
+  showDelete?: boolean
+  editTip?: string
+  deleteTip?: string
+}
+withDefaults(defineProps<PROPS>(), {
+  showEdit: true,
+  showDelete: true,
+  editTip: () => "编辑",
+  deleteTip: () => "删除",
+})
+const emits = defineEmits(["editClick", "deleteClick"])
+</script>
+<template>
+  <n-space justify="center">
+    <slot name="front" />
+    <ga-button
+      v-if="showEdit"
+      :text="true"
+      :is-icon="true"
+      icon-class="i-tabler:edit"
+      type="warning"
+      :is-pop="true"
+      :pop-text="editTip"
+      @exec="emits('editClick')" />
+    <slot name="middle" />
+    <ga-button
+      v-if="showDelete"
+      :text="true"
+      :is-icon="true"
+      icon-class="i-tabler:trash"
+      type="error"
+      :is-pop="true"
+      :pop-text="deleteTip"
+      @exec="emits('deleteClick')" />
+    <slot name="behind" />
+  </n-space>
+</template>
+
+<style></style>
