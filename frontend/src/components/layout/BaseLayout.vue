1eaa86c92f9ca3adab4a34db0a6698cefdf1ee23
jingll <the2ndindec@hotmail.com>
2023-11-10 12:59:10 +0800

    更改布局

--- frontend/src/components/layout/BaseLayout.vue
+++ frontend/src/components/layout/BaseLayout.vue
@@ -2,7 +2,7 @@
 import { NLayout, NLayoutFooter, NButton } from "naive-ui"
 import { useClientHeight } from "/@/composables/client"
 import TheHeader from "/@/components/layout/components/header/TheHeader.vue"
-import TheAside from "/@/components/layout/components/aside/TheAside.vue"
+// import TheAside from "/@/components/layout/components/aside/TheAside.vue"
 const { height } = useClientHeight()
 const handleTest = () => {
   window.$message.info("test")
@@ -13,15 +13,12 @@ const handleTest = () => {
   <n-layout :style="{ height: height + 'px' }">
     <the-header />
     <n-layout position="absolute" style="top: 64px; bottom: 64px" has-sider>
-      <the-aside />
+      <!-- <the-aside /> -->
       <n-layout content-style="padding: 24px;" :native-scrollbar="false">
         <router-view />
       </n-layout>
     </n-layout>
-    <n-layout-footer
-      position="absolute"
-      style="height: 64px; padding: 24px"
-      bordered>
+    <n-layout-footer position="absolute" style="height: 64px; padding: 24px" bordered>
       <n-button size="small" @click="handleTest">城府路</n-button>
     </n-layout-footer>
   </n-layout>
---
+++ frontend/src/components/layout/components/header/HeaderMenu.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { MenuOption, NMenu } from "naive-ui"
+import { h, ref } from "vue"
+import { RouterLink } from "vue-router"
+
+const activeKey = ref<string | null>(null)
+const menuOptions: MenuOption[] = [
+  {
+    label: "系统配置",
+    key: "pinball-1973",
+    children: [
+      {
+        label: () => h(RouterLink, { to: { name: "user" } }, { default: () => "用户管理" }),
+        key: "user",
+      },
+      {
+        label: () => h(RouterLink, { to: { name: "workspace" } }, { default: () => "工作空间" }),
+        key: "workspace",
+      },
+      {
+        label: () => h(RouterLink, { to: { name: "organization" } }, { default: () => "organization" }),
+        key: "organization",
+      },
+    ],
+  },
+  {
+    label: "demo",
+    key: "demo",
+    children: [
+      {
+        label: () => h(RouterLink, { to: { name: "upload" } }, { default: () => "upload" }),
+        key: "user",
+      },
+    ],
+  },
+]
+</script>
+<template>
+  <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" />
+</template>
+
+<style></style>
