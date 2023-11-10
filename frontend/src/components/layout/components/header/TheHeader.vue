efdf0b8a53f5c9bcf886ced47dac927bb0fc983d
jingll <the2ndindec@hotmail.com>
2023-11-10 12:59:52 +0800

    更改布局

--- frontend/src/components/layout/components/header/TheHeader.vue
+++ frontend/src/components/layout/components/header/TheHeader.vue
@@ -1,95 +1,20 @@
 <script setup lang="ts">
-import { NLayoutHeader, NMenu, MenuOption } from "naive-ui"
-import { ref } from "vue"
+import { NLayoutHeader } from "naive-ui"
+import HeaderMenu from "./HeaderMenu.vue"
 import logo from "./../../../../assets/387941.svg"
 import UserInfo from "./UserInfo.vue"
-
-const activeKey = ref<string | null>(null)
-const menuOptions: MenuOption[] = [
-  {
-    label: "1973年的弹珠玩具",
-    key: "pinball-1973",
-
-    disabled: true,
-    children: [
-      {
-        label: "鼠",
-        key: "rat",
-      },
-    ],
-  },
-  {
-    label: "寻羊冒险记",
-    key: "a-wild-sheep-chase",
-
-    disabled: true,
-  },
-  {
-    label: "舞，舞，舞",
-    key: "dance-dance-dance",
-
-    children: [
-      {
-        type: "group",
-        label: "人物",
-        key: "people",
-        children: [
-          {
-            label: "叙事者",
-            key: "narrator",
-          },
-          {
-            label: "羊男",
-            key: "sheep-man",
-          },
-        ],
-      },
-      {
-        label: "饮品",
-        key: "beverage",
-
-        children: [
-          {
-            label: "威士忌",
-            key: "whisky",
-          },
-        ],
-      },
-      {
-        label: "食物",
-        key: "food",
-        children: [
-          {
-            label: "三明治",
-            key: "sandwich",
-          },
-        ],
-      },
-      {
-        label: "过去增多，未来减少",
-        key: "the-past-increases-the-future-recedes",
-      },
-    ],
-  },
-]
 </script>
 <template>
   <n-layout-header
     class="header-layout"
-    style="
-      --side-padding: 32px;
-      grid-template-columns: calc(272px - var(--side-padding)) 1fr auto;
-    "
+    style="--side-padding: 32px; grid-template-columns: calc(272px - var(--side-padding)) 1fr auto"
     bordered>
     <div class="ui-logo">
       <img class="ui-img" :src="logo" />
       <span>Gallywix</span>
     </div>
     <div style="display: flex; align-items: center">
-      <n-menu
-        v-model:value="activeKey"
-        mode="horizontal"
-        :options="menuOptions" />
+      <header-menu />
     </div>
     <div>
       <user-info />
