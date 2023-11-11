86790d0a15f3f2b37be48c4fcfa864079e41ce7a
jingll <the2ndindec@hotmail.com>
2023-11-11 15:43:24 +0800

    :white_check_mark: 自定义debounce指令

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,10 @@ import router from "./router"
 import pinia from "./store"
 import i18n from "./i18n"
 import "virtual:uno.css"
+import debounce from "./directives/debounce"
 
 const app = createApp(App)
 app.use(router).use(pinia).use(i18n)
+//defining the directive
+app.directive("debounce", debounce)
 app.mount("#app")
--- frontend/src/views/demo/upload/index.vue
+++ frontend/src/views/demo/upload/index.vue
@@ -5,11 +5,13 @@ import GaUpload from "/@/components/GaUpload.vue"
 
 const gaUpload = ref<InstanceType<typeof GaUpload> | null>(null)
 const openUpload = () => gaUpload.value?.open()
+const debounceClick = () => window.$message.info("debounceClick")
 </script>
 <template>
   upload
   <h1>{{ $t("home.home") }}</h1>
   <n-button @click="openUpload">上传</n-button>
+  <n-button v-debounce="debounceClick">防抖测试</n-button>
   <ga-upload ref="gaUpload" />
 </template>
 
