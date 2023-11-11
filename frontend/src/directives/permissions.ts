98324b5da0c857b26efc89c4bd22129dda7f29ec
jingll <the2ndindec@hotmail.com>
2023-11-11 16:04:36 +0800

    :white_check_mark: 自定义permission指令

---
+++ frontend/src/directives/permissions.ts
@@ -0,0 +1,34 @@
+import { DirectiveBinding } from "vue"
+import { useAuthStore } from "/@/store/auth-store"
+interface ElType extends HTMLElement {
+  __handleClick__: () => any
+}
+
+const checkPermission = (el: ElType, binding: DirectiveBinding) => {
+  const { value } = binding
+  const store = useAuthStore()
+  const roles = store.roles
+  if (value && value instanceof Array) {
+    if (value.length > 0) {
+      const elRoles = value
+
+      const hasPermission = roles.some((role) => {
+        return elRoles.includes(role)
+      })
+
+      if (!hasPermission) {
+        el.parentNode && el.parentNode.removeChild(el)
+      }
+    }
+  } else {
+    throw new Error(`need a array like ['role1', 'role2']`)
+  }
+}
+export default {
+  mounted(el: ElType, binding: DirectiveBinding) {
+    checkPermission(el, binding)
+  },
+  updated(el: ElType, binding: DirectiveBinding) {
+    checkPermission(el, binding)
+  },
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,9 +6,10 @@ import pinia from "./store"
 import i18n from "./i18n"
 import "virtual:uno.css"
 import debounce from "./directives/debounce"
+import permissions from "./directives/permissions"
 
 const app = createApp(App)
 app.use(router).use(pinia).use(i18n)
 //defining the directive
-app.directive("debounce", debounce)
+app.directive("debounce", debounce).directive("permissions", permissions)
 app.mount("#app")
