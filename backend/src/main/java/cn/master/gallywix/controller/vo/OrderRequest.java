ac84f8f76c580aca586b3bbaffe7b3170a12d467
jingll <the2ndindec@hotmail.com>
2023-11-22 12:26:04 +0800

    OrderRequest

---
+++ backend/src/main/java/cn/master/gallywix/controller/vo/OrderRequest.java
@@ -0,0 +1,52 @@
+package cn.master.gallywix.controller.vo;
+
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author 11's papa
+ * @since 11/16/2023
+ **/
+@Setter
+public class OrderRequest {
+    private String name;
+    private String type;
+    // 表前缀
+    private String prefix;
+
+
+    public String getName() {
+        if (checkSqlInjection(name)) {
+            return "1";
+        }
+        return name;
+    }
+
+    public String getType() {
+        if (StringUtils.equalsIgnoreCase(type, "asc")) {
+            return "ASC";
+        } else {
+            return "DESC";
+        }
+    }
+
+    public String getPrefix() {
+        if (checkSqlInjection(prefix)) {
+            return StringUtils.EMPTY;
+        }
+        return prefix;
+    }
+
+
+    public static boolean checkSqlInjection(String script) {
+        if (StringUtils.isEmpty(script)) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile("^[\\w|\\-]+$");
+        Matcher matcher = pattern.matcher(script.toLowerCase());
+        return !matcher.find();
+    }
+}
