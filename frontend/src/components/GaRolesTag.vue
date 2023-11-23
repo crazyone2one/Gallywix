1853aad15d6c5f41e9f9fb157dfbf61c46cd2ab6
jingll <the2ndindec@hotmail.com>
2023-11-23 14:00:35 +0800

    tag组件

---
+++ frontend/src/components/GaRolesTag.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { IGroupDTO } from "../apis/group"
+import { NTag } from "naive-ui"
+
+interface IProps {
+  roles: Array<IGroupDTO>
+  type?: "default" | "primary" | "info" | "success" | "warning" | "error"
+  size?: "small" | "medium" | "large"
+  bordered?: boolean
+}
+withDefaults(defineProps<IProps>(), {
+  type: "primary",
+  size: "small",
+  bordered: false,
+})
+</script>
+<template>
+  <n-space>
+    <n-tag
+      v-for="(role, index) in roles"
+      :key="index"
+      :type="type"
+      :size="size"
+      :bordered="bordered">
+      {{ role.name }}
+    </n-tag>
+  </n-space>
+</template>
+
+<style></style>
