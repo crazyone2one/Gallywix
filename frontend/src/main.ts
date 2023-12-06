ed0771b5204c97ca09bb311b0bed9711a602127c
jingll <the2ndindec@hotmail.com>
2023-12-06 10:57:41 +0800

    引入element icon

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue"
 
 import App from "./App.vue"
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 
 // import ElementPlus from "element-plus"
 import { setupElementPlus } from "/@/plugins/ele-plus"
@@ -21,4 +22,8 @@ setupElementPlus(app)
 //defining the directive
 app.directive("debounce", debounce)
 app.directive("permission", permissions)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.mount("#app")
